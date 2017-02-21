c0155e74889ad47be381f50efc4033010921e98b
sp0508 <shriyapartha94@gmail.com>
2017-02-21 18:10:08 -0500

    Update Common_cnf.java

--- Common_cnf.java
+++ Common_cnf.java
@@ -1,10 +1,11 @@
+import java.io.FileReader;
 import java.io.Reader;
 import java.util.Properties;
-public class Common_cnf {
+public class Common_cnf 
+{
 	public static final String COMMON_FILENAME = "Common.cfg";
-    public static Properties read (Reader reader) throws Exception {
-    	//Getting the properties from the config file. For now assuming that the file
-    	//is of the default form property=value.
+    public static Properties read () throws Exception {
+    	Reader reader = new FileReader (COMMON_FILENAME);
     	final Properties conf = new Properties ();
         conf.load (reader);
         return conf;
